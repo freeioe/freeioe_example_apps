844ac25a96ac96ff19d066186fbf2b9dee478937
Dirk Chang <dirk@kooiot.com>
2018-08-07 13:02:23 +0800

    merge changes from JC T8600

--- modbus_lua/app_skynet.lua
+++ modbus_lua/app_skynet.lua
@@ -31,6 +31,9 @@ end
 function app:start()
 	--- 设定回调处理函数(目前此应用只做数据采集)
 	self._api:set_handler({
+		on_output = function(app, sn, output, prop, value)
+			return self:write_output(sn, output, prop, value)
+		end,
 		on_ctrl = function(...)
 			print(...)
 		end,
@@ -86,13 +89,22 @@ function app:start()
 				}
 			end
 			inputs[#inputs + 1] = { name = "status", desc = "设备状态", vt="int"}
+			--- outputs
+			local outputs = {}
+			for _, v in ipairs(tpl.outputs) do
+				outputs[#outputs + 1] = {
+					name = v.name,
+					desc = v.desc,
+				}
+			end
 			--- 生成设备对象
-			local dev = self._api:add_device(dev_sn, meta, inputs)
+			local dev = self._api:add_device(dev_sn, meta, inputs, outputs)
 			--- 生成设备通讯口统计对象
 			local stat = dev:stat('port')
 
 			table.insert(self._devs, {
 				unit = v.unit,
+				sn = dev_sn,
 				dev = dev,
 				tpl = tpl,
 				stat = stat,
@@ -118,6 +130,105 @@ function app:close(reason)
 	print(self._name, reason)
 end
 
+function app:write_output(sn, output, prop, value)
+	local dev = nil
+	for _, v in ipairs(self._devs) do
+		if v.sn == sn then
+			dev = v
+			break
+		end
+	end
+	if not dev then
+		return false, "Cannot find device sn "..sn
+	end
+
+	local tpl_output = nil
+	for _, v in ipairs(dev.tpl.outputs or {}) do
+		if v.name == output then
+			tpl_output = v
+			break
+		end
+	end
+	if not tpl_output then
+		return false, "Cannot find output "..sn.."."..output
+	end
+
+	if prop ~= 'value' then
+		return false, "Cannot write property which is not value"
+	end
+	
+	local r, err = self:write_packet(dev.dev, dev.stat, dev.unit, tpl_output, value)
+	if not r then
+		local info = "Write output failure!"
+		local data = { sn=sn, output=output, prop=prop, value=value, err=err }
+		self._dev:fire_event(event.LEVEL_ERROR, event.EVENT_DEV, info, data)
+	end
+	return r, err
+end
+
+function app:write_packet(dev, stat, unit, output, value)
+	--- 设定写数据的地址
+	local req = {
+		func = tonumber(output.func) or 0x06, -- 06指令
+		addr = output.addr, -- 地址
+		unit = unit or output.unit,
+		len = output.len or 1,
+	}
+	local timeout = output.timeout or 500
+	local ef = modbus.encode[output.dt]
+	local df = modbus.decode[output.dt]
+	assert(ef and df)
+
+	local val = math.floor(value * (1/output.rate))
+	req.data = table.concat({ ef(val) })
+	
+	--- 设定通讯口数据回调
+	self._client:set_io_cb(function(io, msg)
+		--- 输出通讯报文
+		dev:dump_comm(io, msg)
+		--- 计算统计信息
+		if io == 'IN' then
+			stat:inc('bytes_in', string.len(msg))
+		else
+			stat:inc('bytes_out', string.len(msg))
+		end
+	end)
+	--- 写入数据
+	local r, pdu, err = pcall(function(req, timeout) 
+		--- 统计数据
+		stat:inc('packets_out', 1)
+		--- 接口调用
+		return self._client:request(req, timeout)
+	end, req, 1000)
+
+	if not r then 
+		pdu = tostring(pdu)
+		if string.find(pdu, 'timeout') then
+			self._log:debug(pdu, err)
+		else
+			self._log:warning(pdu, err)
+		end
+		return nil, pdu
+	end
+
+	if not pdu then 
+		self._log:warning("write failed: " .. err) 
+		return nil, err
+	end
+
+	--- 统计数据
+	stat:inc('packets_in', 1)
+
+	--- 解析数据
+	local pdu_data = string.sub(pdu, 4)
+	local val_ret = df(pdu_data, 1)
+	if val_ret ~= val then
+		return nil, "Write failed!"
+	end
+
+	return true
+end
+
 function app:read_packet(dev, stat, unit, pack)
 	--- 设定读取的起始地址和读取的长度
 	local base_address = pack.saddr or 0x00
@@ -140,6 +251,7 @@ function app:read_packet(dev, stat, unit, pack)
 		end
 	end)
 	--- 读取数据
+	local timeout = pack.timeout or 500
 	local r, pdu, err = pcall(function(req, timeout) 
 		--- 统计数据
 		stat:inc('packets_out', 1)
@@ -168,14 +280,20 @@ function app:read_packet(dev, stat, unit, pack)
 
 	--- 解析数据
 	local d = modbus.decode
+	if d.uint8(pdu, 1) == (0x80 + pack.func) then
+		local basexx = require 'basexx'
+		self._log:warning("read package failed 0x"..basexx.to_hex(string.sub(pdu, 1, 1)))
+		return
+	end
+
 	local len = d.uint8(pdu, 2)
-	--assert(len >= 38 * 2)
+	--assert(len >= pack.len * 2)
+	local pdu_data = string.sub(pdu, 3)
 
 	for _, input in ipairs(pack.inputs) do
 		local df = d[input.dt]
 		assert(df)
-		local index = input.saddr
-		local val = df(pdu, index + 2)
+		local val = df(pdu_data, input.offset)
 		if input.rate and input.rate ~= 1 then
 			val = val * input.rate
 			dev:set_input_prop(input.name, "value", val)
