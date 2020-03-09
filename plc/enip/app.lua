9cbc7fc4c816be8bf5123ec0626275c3c74f83f4
Dirk Chang <dirk@kooiot.com>
2020-03-09 16:11:26 +0800

    show the error information

--- plc/enip/app.lua
+++ plc/enip/app.lua
@@ -61,7 +61,11 @@ function app:on_run(tms)
 
 	for _, v in ipairs(self._inputs) do
 		local r, err = self._client:read_tag(v.name, v.dt, function(val, err)
-			self._log:debug('Value from PLC', val, err)
+			if not val then
+				self._log:error('Read PLC tag error:', err)
+			else
+				self._log:debug('Value from PLC', val, err)
+			end
 			if val then
 				self._dev:set_input_prop(v.name, 'value', val)
 			else
