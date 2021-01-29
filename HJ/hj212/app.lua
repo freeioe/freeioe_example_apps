f2807ba61e8fc5995e84e9da464441afb72d3c58
Dirk Chang <dirk@kooiot.com>
2021-01-29 20:09:57 +0800

    update hj212 stack version

--- HJ/hj212/app.lua
+++ HJ/hj212/app.lua
@@ -17,7 +17,7 @@ local conn = require 'conn'
 local tag = require 'hjtag'
 local hisdb = require 'hisdb.hisdb'
 
---- lua_HJ212_version: 2021-01-28
+--- lua_HJ212_version: 2021-01-29
 
 --- 注册对象(请尽量使用唯一的标识字符串)
 local app = app_base:subclass("FREEIOE_HJ212_APP")
