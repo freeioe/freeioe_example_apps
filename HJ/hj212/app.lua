39e7bd0b9f0a7a2d731d42ef5d1d6aaeeaf0e5bc
Dirk Chang <dirk@kooiot.com>
2021-01-30 13:42:39 +0800

    correct typo for pollution

--- HJ/hj212/app.lua
+++ HJ/hj212/app.lua
@@ -18,7 +18,7 @@ local tag = require 'hjtag'
 local hisdb = require 'hisdb.hisdb'
 
 --- lua_HJ212_version: 2021-01-30
---  comment: Hnadle the _z values in pullut tags
+--  comment: Hnadle the _z values in pollution tags
 
 --- 注册对象(请尽量使用唯一的标识字符串)
 local app = app_base:subclass("FREEIOE_HJ212_APP")
