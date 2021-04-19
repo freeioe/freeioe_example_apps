3535fe61647e3f6b6e1d28754dabd2622dbcb97a
Dirk Chang <dirk@kooiot.com>
2021-04-19 16:20:13 +0800

    fixed settings not updated issue

--- HJ/hebei/app.lua
+++ HJ/hebei/app.lua
@@ -578,6 +578,9 @@ function app:on_run(tms)
 		end
 	end
 
+	local settings = ioe.env.get('HJ212.SETTINGS', conf.station)
+	self._station:set_settings(settings)
+
 	-- TODO: Update status
 
 	return 1000
